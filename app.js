db8bd3ce1ee64f881335854486aa2b0b89d71274
Tucker Hoog <tuckerhoog@tutanota.com>
2021-02-12 17:49:53 -0500

    ids for inputs and styling changes

--- app.js
+++ app.js
@@ -3,3 +3,18 @@
 // initialize state
 
 // set event listeners to update state and DOM
+
+const noun-1;
+const noun-2;
+const noun-3;
+const noun-4;
+
+const verb-1;
+const verb-2;
+const verb-3;
+const verb-4;
+
+const adjective-1;
+const adjective-2;
+const adjective-3;
+const adjective-4;
