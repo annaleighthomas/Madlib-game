7ac6716b9557819470bb7491431081dd3b12d07d
Tucker Hoog <tuckerhoog@tutanota.com>
2021-02-12 19:01:52 -0500

    added stuff

--- app.js
+++ app.js
@@ -4,20 +4,21 @@
 
 // set event listeners to update state and DOM
 
+const submitButton = document.getElementById('submit-blanks');
 
+const nounOne = document.getElementById('noun-1');
+const place = document.getElementById('place');
+const personThing = document.getElementById('person-thing');
+const nounTwo = document.getElementById('noun-2');
+const nounThree = document.getElementById('noun-3');
+const nounFour = document.getElementById('noun-4');
+const verbOne = document.getElementById('verb-1');
+const verbTwo = document.getElementById('verb-2');
+const nounFive = document.getElementById('noun-5');
+const adjective = document.getElementById('adjective');
 
-const nounOne = document.getElementById('noun-1')
-const nounTwo = document.getElementById('noun-2')
-const nounThree = document.getElementById('noun-3')
-const nounFour = document.getElementById('noun-4')
-const nounFive = document.getElementById('noun-5')
+const answerOne = document.getElementById('answer-one');
 
-const verbOne;
-const verbTwo;
-const verbThree;
-const verbFour;
-
-const adjectiveOne;
-const adjectiveTwo;
-const adjectiveThree;
-const adjectiveFour;
+submitButton.addEventListener('click', () => {
+    answerOne.textContent = nounOne.value;
+});
